cd46699a428290c00f800764ec1bcbbdb7927f91
jinwoooooooo <jinwooya55@gmail.com>
2025-05-28 12:27:31 +0900

    farmStory Backend

--- src/main/java/kr/co/farmstoryapi/repository/TermsRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.farmstoryapi.repository;
-
-import kr.co.farmstoryapi.entity.Terms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TermsRepository extends JpaRepository<Terms, Integer> {
-}
